cbc433981526eeb0a5bff1639c141bf0d317a74d
Sanjit24505 <sanjit24505@iiitd.ac.in>
2025-02-19 20:58:07 +0530

    Update assembler.py

--- SimpleAssembler/assembler.py
+++ SimpleAssembler/assembler.py
@@ -130,3 +130,26 @@ def code_parse(assembly_lines):
             instructions.append(line)
             current_address += 4
     return labels, instructions
+
+def machine_code(instuctions, labels, lineNo):
+    instruction_fields = re.split(r'[ ,]+', instuctions.strip())
+    inst = instruction_fields[0]
+    if inst not in opcode.keys():
+        return f"Error: Invalid instruction {inst} at line {lineNo}"
+    if inst == 'addi' and instruction_fields == 'addi x0, x0, 0':
+        return '00000000000000000000000000000000' 
+    if inst == 'halt':
+        return '11111111111111111111111111111111'
+    if inst in ("add", "sub", "slt","srl", "or", "and"):
+        if len(instruction_fields)!=4:
+            return f"Syntax Error at line {lineNo}"
+
+        rd = RegToNum(instruction_fields[1])
+        rs1 = RegToNum(instruction_fields[2])
+        rs2 = RegToNum(instruction_fields[3])
+        lis1=[rd,rs1,rs2]
+
+        for i in lis1:
+            if i==-1:
+                return f"Error: Invalid register name at line {lineNo}"
+        return R_type(inst, rd, rs1, rs2)
